404dfb1e76653d734499062c1885b6e6f20ca02b
Thy Ai <ainguyen@192.168.2.7>
2024-11-18 00:11:15 +0700

    feat: Login and forgot password

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  timeout: 50 * 6 * 100,
+  timeout: 50 * 60 * 1000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
